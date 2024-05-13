b9ca5138c51d0db5dc123e718ae27776be8f5e9d
Tommaso Dossena <dossena.tommaso.dt@gmail.com>
2024-05-13 15:25:25 +0200

    feat: add dotted background

--- src/app/(platform)/boards/layout.tsx
+++ src/app/(platform)/boards/layout.tsx
@@ -19,9 +19,10 @@ export default function BoardsLayout({ children }: BoardsLayoutProps) {
       disableTransitionOnChange
     >
       <TooltipProvider delayDuration={0}>
-        <div className="grid grid-cols-[auto,minmax(0,1fr)] w-full">
-          <Sidebar className="h-screen" />
-          <main className="h-screen">
+        <div className="grid grid-cols-[auto,minmax(0,1fr)] w-full dark:bg-black bg-white dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative">
+          <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+          <Sidebar className="h-screen z-20" />
+          <main className="h-screen z-10">
             {children}
           </main>
         </div>
--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -67,7 +67,7 @@ export function DataTable<TData, TValue>({
   return (
     <div className="space-y-4 mt-3 pl-2 pr-3">
       <DataTableToolbar table={table} />
-      <div className="rounded-md border">
+      <div className="rounded-md border bg-primary-foreground">
         <ScrollArea>
           <Table>
             <TableHeader>
